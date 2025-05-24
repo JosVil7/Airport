f52da441e214035a0835cc40961ce90e938d8bcc
I4ma1k <iblanquiceth@uninorte.edu.co>
2025-05-23 22:13:12 -0500

    Cargador de datos terminado y arreglado problemitas con Storage_Flight

--- src/airport/models/database/D_Loader.java
+++ src/airport/models/database/D_Loader.java
@@ -4,10 +4,59 @@
  */
 package airport.models.database;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.json.JSONArray;
+
 /**
  *
  * @author USER
  */
 public class D_Loader {
-    //hola
+    public static void load_Planes(){
+        try{
+            String texto = new String(Files.readAllBytes(Paths.get("json/planes.json")));
+            JSONArray array = new JSONArray(texto);
+            Storage_Plane.getInstance().cargarJSON(array);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void load_Locations(){
+        try{
+            String texto = new String(Files.readAllBytes(Paths.get("json/locations.json")));
+            JSONArray array = new JSONArray(texto);
+            Storage_Location.getInstance().cargarJSON(array);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void load_Passengers(){
+        try{
+            String texto = new String(Files.readAllBytes(Paths.get("json/passengers.json")));
+            JSONArray array = new JSONArray(texto);
+            Storage_Passenger.getInstance().cargarJSON(array);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void load_Flights(){
+        try{
+            String texto = new String(Files.readAllBytes(Paths.get("json/flights.json")));
+            JSONArray array = new JSONArray(texto);
+            Storage_Flight.getInstance().cargarJSON(array);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void loadALL(){
+        load_Planes();
+        load_Locations();
+        load_Passengers();
+        load_Flights();
+    }
 }
--- src/airport/models/database/Storage_Flight.java
+++ src/airport/models/database/Storage_Flight.java
@@ -5,7 +5,9 @@
 package airport.models.database;
 
 import airport.models.Flight;
+import airport.models.Location;
 import airport.models.Plane;
+import java.time.LocalDateTime;
 
 
 import java.util.ArrayList;
@@ -69,6 +71,20 @@ public class Storage_Flight {
             String plane = objecto.getString("plane");
             //problemita con getplane
             Plane plane_real = Storage_Plane.getInstance().getPlane(plane);
+            String departureLocation = objecto.getString("departureLocation");
+            Location departureLocation_real = Storage_Location.getInstance().getLocation(departureLocation);
+            String scaleLocation = objecto.getString("scaleLocation");
+            Location scaleLocation_real = Storage_Location.getInstance().getLocation(scaleLocation);
+            String arrivalLocation = objecto.getString("arrivalLocation");
+            Location arrivalLocation_real = Storage_Location.getInstance().getLocation(arrivalLocation);
+            String departureDate_str = objecto.getString("departureDate");
+            LocalDateTime departureDate = LocalDateTime.parse(departureDate_str);
+            int hoursDurationArrival = objecto.getInt("hoursDurationArrival");
+            int minutesDurationArrival = objecto.getInt("minutesDurationArrival");
+            int hoursDurationScale = objecto.getInt("hoursDurationScale");
+            int minutesDurationScale = objecto.getInt("minutesDurationScale");
+            
+            Flight flight = new Flight(id, plane_real, departureLocation_real, scaleLocation_real, arrivalLocation_real, departureDate, hoursDurationArrival, minutesDurationArrival, hoursDurationScale, minutesDurationScale);
             
         }
     }
